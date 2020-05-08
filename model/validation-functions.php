70575eccc11325037fa1082e18f9b176025ccb89
Michael Gulchuk <mgulchuk@mail.greenriver.edu>
2020-05-08 16:55:07 -0400

    Added model directoy

---
+++ model/validation-functions.php
@@ -0,0 +1,20 @@
+<?php
+
+/* Validate a color
+*
+* @param String color
+* @return boolean
+*/
+function validColor($color)
+{
+    global $f3;
+    return in_array($color, $f3->get('colors'));
+}
+
+function validString($string){
+    if(is_string($string)){
+        return true;
+    }else{
+        return false;
+    }
+}

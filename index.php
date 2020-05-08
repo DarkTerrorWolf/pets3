08657f91c33c8f4caa0b4138752760234b437a3c
Tylor Schlosser <tschlosser@mail.greenriver.edu>
2020-05-08 17:23:05 -0400

    validation added

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 require_once('vendor/autoload.php');
+require_once('model/validation-functions.php');
 
 //F3 class
 $f3 = Base::instance();
@@ -17,17 +18,31 @@ $f3->route('GET /', function () {
     echo $view->render("views/home.html");
 }
 );
-$f3->route('GET|POST /order', function () {
-    //echo "<h1>Hello World!</h1>";
-    $view = new Template();
-    echo $view->render("views/orderform.html");
+$f3->route('GET|POST /order', function ($f3) {
+    $colors = array('Red','Blue','Brown','Pink','Black');
+    $f3 ->set('colors',$colors);
+    session_start();
+    $_SESSION = array();
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $animal = $_POST['animal'];
+        $color = $_POST['color'];
+        if (validString($animal)&& validColor($color)) {
+            $_SESSION['animal'] = $animal;
+            $_SESSION['color'] = $color;
+            $f3->reroute('results');
+        } else {
+            $f3->set("errors['animal']", "Please enter an animal.");
+        }
+    }
+    $template = new Template();
+    echo $template->render('views/orderform.html');
 }
 );
-
-$f3->route('GET|POST /form2', function () {
+$f3->route('GET /results', function () {
     //echo "<h1>Hello World!</h1>";
     $view = new Template();
-    echo $view->render("views/form2.html");
+    echo $view->render("views/results.html");
+    session_destroy();
 }
 );
 
